f782e6a72f39ad764e4c33e81d67760b29a1d949
Endriu12 <github2017>
2017-08-24 23:02:30 +0300

    JsonManafer fixed

--- src/main/java/com/perepelitsya/service/impls/JsonManagerService.java
+++ src/main/java/com/perepelitsya/service/impls/JsonManagerService.java
@@ -6,16 +6,12 @@ import com.perepelitsya.model.Student;
 import com.perepelitsya.model.Subject;
 import com.perepelitsya.service.interfaces.FileManager;
 import org.apache.log4j.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.*;
-import java.time.LocalDateTime;
+import java.io.File;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 
@@ -30,8 +26,7 @@ public class JsonManagerService implements FileManager {
     DateTimeFormatter formatterDateForBirthday = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     DateTimeFormatter formatterDateForMark = DateTimeFormatter.ofPattern("MM-dd HH:mm");
 
-
-//
+    //
 //    @Override
 //    public void writeToFileStudent(List<Student> studentList) throws IOException {
 ////
@@ -64,32 +59,37 @@ public class JsonManagerService implements FileManager {
 //        }
 //
 //    }
-
     @Override
     public void writeToFileStudent(List<Student> studentList) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.writeValue(new File(studentFile), studentList);
+        log.info("Students wrote to file");
     }
 
     @Override
     public void writeToFileSubject(List<Subject> subjectList) throws IOException {
-        for (Subject subject : subjectList) {
-            JSONObject obj = new JSONObject();
-            obj.put("name", subject.getName());
-            obj.put("idOfSubject", subject.getIdOfSubject());
-            try (FileWriter file = new FileWriter(subjectFile)) {
-                file.write(obj.toJSONString());
-                file.flush();
-                log.info("Json subject created");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+//        for (Subject subject : subjectList) {
+//            JSONObject obj = new JSONObject();
+//            obj.put("name", subject.getName());
+//            obj.put("idOfSubject", subject.getIdOfSubject());
+//            try (FileWriter file = new FileWriter(subjectFile)) {
+//                file.write(obj.toJSONString());
+//                file.flush();
+//                log.info("Json subject created");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(subjectFile), subjectList);
+        log.info("Subjects wrote to file");
+
     }
 
     @Override
     public List<Student> readFromFileStudent() throws IOException, ParseException {
-//        List<Student> studentList = new ArrayList<>();
+//
+//                List<Student> studentList = new ArrayList<>();
 //        try {
 //            FileReader reader = new FileReader(studentFile);
 //            JSONParser jsonParser = new JSONParser();
@@ -118,25 +118,29 @@ public class JsonManagerService implements FileManager {
 //            ex.printStackTrace();
 //        }
         ObjectMapper objectMapper = new ObjectMapper();
-        final CollectionLikeType listType = objectMapper.getTypeFactory().constructCollectionLikeType(List.class, Student.class);
-        List<Student> students = objectMapper.readValue(studentFile, listType);
+        final CollectionLikeType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, Student.class);
+        List<Student> students = objectMapper.readValue(new File(studentFile), listType);
         return students;
     }
 
     @Override
     public List<Subject> readFromFileSubject() throws IOException, ParseException {
-
+//        List<Subject> subjectList = new ArrayList<>();
+//        try {
+//            FileReader fileReader = new FileReader(subjectFile);
+//            JSONParser jsonParser = new JSONParser();
+//            JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
+//            long id = (long) jsonObject.get("idOfSubject");
+//            String name = (String) jsonObject.get("name");
+//            subjectList.add(new Subject(id, name));
+//        } catch (FileNotFoundException | ClassCastException e) {
+//            e.printStackTrace();
+//        }
+//        return subjectList;
+        ObjectMapper objectMapper = new ObjectMapper();
         List<Subject> subjectList = new ArrayList<>();
-        try {
-            FileReader fileReader = new FileReader(subjectFile);
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
-            long id = (long) jsonObject.get("idOfSubject");
-            String name = (String) jsonObject.get("name");
-            subjectList.add(new Subject(id, name));
-        } catch (FileNotFoundException | ClassCastException e) {
-            e.printStackTrace();
-        }
+        final CollectionLikeType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, Subject.class);
+        subjectList.add(objectMapper.readValue(new File(subjectFile), listType));
         return subjectList;
     }
 
