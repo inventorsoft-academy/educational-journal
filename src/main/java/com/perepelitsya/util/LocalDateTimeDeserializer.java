5c13de1aa5aa8998db25110bdc686a9d7093c65d
Endriu12 <github2017>
2017-08-26 23:05:21 +0300

    Updated

--- src/main/java/com/perepelitsya/util/LocalDateTimeDeserializer.java
+++ src/main/java/com/perepelitsya/util/LocalDateTimeDeserializer.java
@@ -1,5 +1,6 @@
 package com.perepelitsya.util;
 
+
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -7,13 +8,24 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Andriu on 8/24/2017.
  */
 public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+//    DateTimeFormatter formatterDateForBirthday = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//    DateTimeFormatter formatterDateForMark = DateTimeFormatter.ofPattern("MM-dd HH:mm");
+
     @Override
-    public LocalDateTime deserialize(JsonParser arg0, DeserializationContext arg1) throws IOException, JsonProcessingException {
-        return LocalDateTime.parse(arg0.getText());
+    public LocalDateTime deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
+        return LocalDateTime.parse(parser.readValueAs(String.class));
+//
+//        return LocalDate.parse(parser.readValueAs(String.class));
+
+
     }
 }
+
+
--- src/main/java/com/perepelitsya/util/LocalDateTimeSerializer.java
+++ src/main/java/com/perepelitsya/util/LocalDateTimeSerializer.java
@@ -7,13 +7,16 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Andriu on 8/24/2017.
  */
 public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+    DateTimeFormatter formatterDateForBirthday = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     @Override
-    public void serialize(LocalDateTime arg0, JsonGenerator arg1, SerializerProvider arg2) throws IOException, JsonProcessingException {
-        arg1.writeString(arg0.toString());
+
+    public void serialize(LocalDateTime date, JsonGenerator gen, SerializerProvider provider) throws IOException, JsonProcessingException {
+        gen.writeString(date.format(formatterDateForBirthday));
     }
 }
