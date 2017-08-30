5733e08fbb8a435da1bab8925fb84b419f50fd12
Endriu12 <github2017>
2017-08-30 13:20:38 +0300

    refactor method addMarkToStudent

---
+++ src/main/java/com/perepelitsya/service/interfaces/MarkManager.java
@@ -0,0 +1,13 @@
+package com.perepelitsya.service.interfaces;
+
+import com.perepelitsya.model.Mark;
+
+/**
+ * Created by Andriu on 8/29/2017.
+ */
+public interface MarkManager {
+
+//    void saveMark(Mark mark);
+
+    public void addMarksToStudent(long id, long subject_id, int mark)
+}
